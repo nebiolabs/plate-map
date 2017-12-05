2c67677956d26bddafdb779d557ec0d9e5606957
Victor Shen <Victor.Shen@tessella.com>
2017-12-05 17:33:53 -0500

    refactor

--- app/assets/javascripts/plate-map/add-tab-data.js
+++ app/assets/javascripts/plate-map/add-tab-data.js
@@ -128,16 +128,6 @@ var plateLayOutWidget = plateLayOutWidget || {};
             that._addAllData(data);
           };
           if (data.type === "multiselect"){
-            field.removeAllSelectedVal = function() {
-              var valToRemove = field.curToRemoveVal;
-              if (valToRemove){
-                for (var idx in valToRemove) {
-                  var val = valToRemove[idx];
-                  field.multiOnChange(null, {id: val})
-                }
-              }
-            };
-
             that._createDeleteButton(field, tabPointer);
           }
           return field;
@@ -242,7 +232,6 @@ var plateLayOutWidget = plateLayOutWidget || {};
           };
 
         });
-
         field.getValue = function(){
           var v = field.input.select2('data');
           if (v.length) {
@@ -253,6 +242,7 @@ var plateLayOutWidget = plateLayOutWidget || {};
           return null;
         };
 
+        // Add delete pop up for multiplex field
         that._createDeleteButton(field, tabPointer);
 
         return field;
@@ -295,180 +285,139 @@ var plateLayOutWidget = plateLayOutWidget || {};
         });
         */
 
-        that._popUp(field, deleteButton);
+        createPopUp(field, deleteButton);
 
-      },
+        function createPopUp(field, button) {
+          var obj = $('#my-plate-layout');
 
-      _popUp: function(field, button) {
-        var that = this;
-        var obj = $('#my-plate-layout');
-
-        button.click(function(evt) {
-          if ($('#applyDialog').length == 0) {
-            $('body').append("<div id='applyDialog'><div/>");
-          }
-          var dialogDiv = $('#applyDialog').addClass("modal");
-          var dialogContent = $("<div/>").addClass("modal-content");
-          dialogDiv.append(dialogContent);
-
-          var popUpContent = that._popUpContent(field);
-          dialogContent.append(popUpContent.tabelDiv);
-
-          var buttonRow = $("<div/>").addClass("dialog-buttons").css("justify-content", "flex-end");
-          dialogContent.append(buttonRow);
-          var deleteCheckedButton = $("<button>Delete Checked Items</button>");
-          buttonRow.append(deleteCheckedButton);
-
-          deleteCheckedButton.click(function() {
-            var optMap = popUpContent.optionMap;
-            var idToRemove = [];
-            for (var idx in Object.keys(optMap)) {
-              var optId = Object.keys(optMap)[idx];
-              var divId = "#"+ "checkBoxId" + idx;
-              if ($(divId)[0].checked){
-                field.data.options.forEach(function(opt) {
-                  if (opt.text === optId){
-                    idToRemove.push(opt.id)
-                  }
-                })
-              }
+          button.click(function(evt) {
+            if ($('#applyDialog').length == 0) {
+              $('body').append("<div id='applyDialog'><div/>");
             }
+            var dialogDiv = $('#applyDialog').addClass("modal");
+            var dialogContent = $("<div/>").addClass("modal-content");
+            dialogDiv.append(dialogContent);
 
-            if (idToRemove.length > 0){
-              for (var idx in idToRemove) {
-                var val = idToRemove[idx];
-                field.multiOnChange(null, {id: val})
+            var popUpContent = createPopUpContent(field);
+            dialogContent.append(popUpContent.tabelDiv);
+
+            var buttonRow = $("<div/>").addClass("dialog-buttons").css("justify-content", "flex-end");
+            dialogContent.append(buttonRow);
+            var deleteCheckedButton = $("<button>Delete Checked Items</button>");
+            buttonRow.append(deleteCheckedButton);
+
+            deleteCheckedButton.click(function() {
+              var optMap = popUpContent.optionMap;
+              var idToRemove = [];
+              for (var idx in Object.keys(optMap)) {
+                var optId = Object.keys(optMap)[idx];
+                var divId = "#"+ "checkBoxId" + idx;
+                if ($(divId)[0].checked){
+                  field.data.options.forEach(function(opt) {
+                    if (opt.text === optId){
+                      idToRemove.push(opt.id)
+                    }
+                  })
+                }
               }
-            }
 
-            // refresh selected fields after updating the multiplex field value
-            that.decideSelectedFields();
+              if (idToRemove.length > 0){
+                for (var idx in idToRemove) {
+                  var val = idToRemove[idx];
+                  field.multiOnChange(null, {id: val})
+                }
+              }
 
-            dialogDiv.hide();
-            $('#applyDialog').remove();
+              // refresh selected fields after updating the multiplex field value
+              that.decideSelectedFields();
 
-          });
-
-          var cancelButton = $("<button>Cancel</button>");
-          buttonRow.append(cancelButton);
-          cancelButton.click(function () {
-            dialogDiv.hide();
-            $('#applyDialog').remove();
-          });
-          dialogDiv.show();
-
-          window.onclick = function(event) {
-
-            if (event.target == dialogDiv[0]) {
               dialogDiv.hide();
               $('#applyDialog').remove();
+
+            });
+
+            var cancelButton = $("<button>Cancel</button>");
+            buttonRow.append(cancelButton);
+            cancelButton.click(function () {
+              dialogDiv.hide();
+              $('#applyDialog').remove();
+            });
+            dialogDiv.show();
+
+            window.onclick = function(event) {
+
+              if (event.target == dialogDiv[0]) {
+                dialogDiv.hide();
+                $('#applyDialog').remove();
+              }
             }
-          }
-        });
-      },
-
-      _popUpContent: function(field) {
-        var that = this;
-        var valMap = field.curToRemoveVal;
-        var valToRemove = Object.keys(valMap);
-
-        var optionMap = {};
-
-        valToRemove.forEach(function(valId){
-          var val = valMap[valId];
-          field.data.options.forEach(function(opt){
-            if (opt.id.toString() === valId.toString()) {
-              optionMap[opt.text] = val;
-            }
-          })
-        });
-
-        var tableArea;
-        var table;
-        var thead;
-        var tr;
-        var th;
-
-        var data = [field.name, "Count", "Delete"]; //Added because it was missing... no idea what the original should have been
-
-        tableArea = that._createElement("<div></div>");
-        table = document.createElement('table');
-        thead = document.createElement('thead');
-        tr = document.createElement('tr');
-
-        for (var i = 0; i < data.length; i++) {
-          var headerTxt = document.createTextNode(data[i]);
-          th = document.createElement('th');
-          th.appendChild(headerTxt);
-          tr.appendChild(th);
-          thead.appendChild(tr);
+          });
         }
 
-        table.appendChild(thead);
-
-        for (var idx in Object.keys(optionMap)) {
-          var optId = Object.keys(optionMap)[idx];
-          tr = document.createElement('tr');
-          tr.appendChild(document.createElement('td'));
-          tr.appendChild(document.createElement('td'));
-          tr.appendChild(document.createElement('td'));
-
-          var checkbox = document.createElement("INPUT"); //Added for checkbox
-          checkbox.type = "checkbox"; //Added for checkbox
-          checkbox.id = "checkBoxId" + idx;
-
-          tr.cells[0].appendChild(document.createTextNode(optId));
-          tr.cells[1].appendChild(document.createTextNode(optionMap[optId]));
-          tr.cells[2].appendChild(checkbox); //Added for checkbox
-
-          table.appendChild(tr);
-        }
-        tableArea.append(table);
-        return {
-          tabelDiv: tableArea,
-          optionMap: optionMap
-        };
-
-      }
-
-      // backup
-      /*
-      _createDeleteButton: function(field, tabPointer){
-        var that = this;
-        var obj = $('#my-plate-layout');
-        var deleteButton = $("<button/>").addClass("plate-setup-remove-all-button");
-
-        deleteButton.text("Delete " + field.name + " in all selected wells");
-
-        var wrapperDiv = that._createElement("<div></div>").addClass("plate-setup-tab-default-field");
-        var wrapperDivLeftSide = that._createElement("<div></div>").addClass("plate-setup-tab-field-left-side");
-        var wrapperDivRightSide = that._createElement("<div></div>").addClass("plate-setup-tab-field-right-side ");
-
-        var buttonContainer = that._createElement("<div></div>").addClass("plate-setup-remove-all-button-container");
-        buttonContainer.append(deleteButton);
-
-        $(wrapperDivRightSide).append(buttonContainer);
-        $(wrapperDiv).append(wrapperDivLeftSide);
-        $(wrapperDiv).append(wrapperDivRightSide);
-
-
-        $(that.allDataTabs[tabPointer]).append(wrapperDiv);
-
-        deleteButton.click(function(evt) {
+        function createPopUpContent(field) {
           var valMap = field.curToRemoveVal;
           var valToRemove = Object.keys(valMap);
-          if (valToRemove){
-            for (var idx in valToRemove) {
-              var val = valToRemove[idx];
-              field.multiOnChange(null, {id: val})
-            }
+
+          var optionMap = {};
+
+          valToRemove.forEach(function(valId){
+            var val = valMap[valId];
+            field.data.options.forEach(function(opt){
+              if (opt.id.toString() === valId.toString()) {
+                optionMap[opt.text] = val;
+              }
+            })
+          });
+
+          var tableArea;
+          var table;
+          var thead;
+          var tr;
+          var th;
+
+          var data = [field.name, "Count", "Delete"]; //Added because it was missing... no idea what the original should have been
+
+          tableArea = that._createElement("<div></div>");
+          table = document.createElement('table');
+          thead = document.createElement('thead');
+          tr = document.createElement('tr');
+
+          for (var i = 0; i < data.length; i++) {
+            var headerTxt = document.createTextNode(data[i]);
+            th = document.createElement('th');
+            th.appendChild(headerTxt);
+            tr.appendChild(th);
+            thead.appendChild(tr);
           }
 
-          // refresh selected fields after updating the multiplex field value
-          that.decideSelectedFields();
-        });
+          table.appendChild(thead);
+
+          for (var idx in Object.keys(optionMap)) {
+            var optId = Object.keys(optionMap)[idx];
+            tr = document.createElement('tr');
+            tr.appendChild(document.createElement('td'));
+            tr.appendChild(document.createElement('td'));
+            tr.appendChild(document.createElement('td'));
+
+            var checkbox = document.createElement("INPUT"); //Added for checkbox
+            checkbox.type = "checkbox"; //Added for checkbox
+            checkbox.id = "checkBoxId" + idx;
+
+            tr.cells[0].appendChild(document.createTextNode(optId));
+            tr.cells[1].appendChild(document.createTextNode(optionMap[optId]));
+            tr.cells[2].appendChild(checkbox); //Added for checkbox
+
+            table.appendChild(tr);
+          }
+          tableArea.append(table);
+          return {
+            tabelDiv: tableArea,
+            optionMap: optionMap
+          };
+
+        }
       }
-      */
+
     }
   }
 
