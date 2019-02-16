741ed0ab5dc6ff7c2266157e3c92e69d68a8f6c7
Jeremy Gore <jeremy.gore@tessella.com>
2019-02-16 01:25:31 -0500

    Build code

--- dist/js/plate-map.js
+++ dist/js/plate-map.js
@@ -2096,7 +2096,7 @@ var plateLayOutWidget = plateLayOutWidget || {};
               selected_v = null;
             }
           }
-          field.singleSelect.select2('data', {});
+          field.singleSelect.select2('data', []);
           field.singleSelect.select2(opts);
           field.singleSelect.select2('data', selected_v);
           field.singleSelect.prop("disabled", opts.data.length == 0);
@@ -2130,6 +2130,7 @@ var plateLayOutWidget = plateLayOutWidget || {};
           that.readOnlyHandler();
         };
 
+        field.singleSelect.select2();
         setSingleSelectOptions([]);
 
         field.singleSelect.on("change", singleSelectChange);
